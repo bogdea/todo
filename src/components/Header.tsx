628484421b8e9ec719023d07cb820345bb3bf976
Madalin Bogdea <madalinbogdea@icloud.com>
2024-09-23 10:10:13 +0300

    create header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Logo from "./ui/Logo.tsx";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <Logo logo="todo" />
+    </div>
+  );
 };
 
 export default Header;
