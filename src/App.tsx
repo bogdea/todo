9e3020e6401d0e86ebf0bbf5206fa843850c5eeb
Madalin Bogdea <madalinbogdea@icloud.com>
2024-09-23 10:15:15 +0300

    add vertical spacing to container

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div className="container max-w-[500px]">
+    <div className="container my-5 max-w-[500px]">
       <Header />
     </div>
   );
