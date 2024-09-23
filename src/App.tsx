6afb9d8e2c72f8c0f42f17ae21aa15a63f47cae4
Madalin Bogdea <madalinbogdea@icloud.com>
2024-09-23 10:10:39 +0300

    import header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "./components/Header";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="container max-w-[500px]">
+      <Header />
+    </div>
+  );
 };
 
 export default App;
