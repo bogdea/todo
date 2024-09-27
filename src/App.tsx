c2b4b2edb7c197fdcc78ff545013ccb157ab6093
Madalin Bogdea <madalinbogdea@icloud.com>
2024-09-27 12:48:54 +0300

    implement tasks section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 const App = () => {
   return (
     <div className="container my-5 max-w-[500px]">
       <Header />
+      <Tasks />
     </div>
   );
 };
