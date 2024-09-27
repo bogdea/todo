d6c4b4a99929e41f18c28ff98a61ae5d4dea86ea
Madalin Bogdea <madalinbogdea@icloud.com>
2024-09-27 13:09:04 +0300

    implement addtask component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/ui/AddTask";
 
 const App = () => {
   return (
     <div className="container my-5 max-w-[500px]">
       <Header />
       <Tasks />
+      <AddTask />
     </div>
   );
 };
