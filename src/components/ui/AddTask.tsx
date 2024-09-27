77af6903fc27f98bf101cf1e1994441228f0fc82
Madalin Bogdea <madalinbogdea@icloud.com>
2024-09-27 13:08:45 +0300

    create addtask component

---
+++ src/components/ui/AddTask.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddTask = () => {
+  return (
+    <div className="bg-oceanBlue mt-4 cursor-pointer rounded-lg p-1 text-center text-sm font-semibold text-white">
+      add task
+    </div>
+  );
+};
+
+export default AddTask;
